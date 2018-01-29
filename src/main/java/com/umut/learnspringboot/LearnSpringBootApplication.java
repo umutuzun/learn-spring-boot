e9f948e30a93f47a4558592f812a3e6d46fe7c14
Umut Uzun <umutu@evam.com>
2018-01-29 21:29:34 +0300

    Add demo spring boot project

---
+++ src/main/java/com/umut/learnspringboot/LearnSpringBootApplication.java
@@ -0,0 +1,43 @@
+package com.umut.learnspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class LearnSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringBootApplication.class, args);
+	}
+    @RestController
+	class Resource {
+	    @RequestMapping(
+	            method = RequestMethod.GET
+        )
+	    Message getMessage(){
+	        return new Message("Hello World");
+        }
+    }
+
+	class Message {
+	    private final String message;
+
+        public Message(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        @Override
+        public String toString() {
+            return "Message{" +
+                    "message='" + message + '\'' +
+                    '}';
+        }
+    }
+}
