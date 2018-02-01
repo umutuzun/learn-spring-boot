466ead950247301fee51db916313e0fa505bb2bf
Umut Uzun <umutu@evam.com>
2018-02-01 14:01:12 +0300

    Add resource layer

---
+++ src/main/java/com/umut/learnspringboot/resource/UserResource.java
@@ -0,0 +1,45 @@
+package com.umut.learnspringboot.resource;
+
+import com.umut.learnspringboot.model.User;
+import com.umut.learnspringboot.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Created by Umut Uzun on 2/1/2018.
+ */
+
+@RestController
+@RequestMapping(
+        path = "api/v1/users"
+)
+public class UserResource {
+
+    private UserService userService;
+
+    @Autowired
+    public UserResource(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping(
+            method = RequestMethod.GET
+    )
+    public List<User> fetchUsers() {
+        return userService.getAllUsers();
+    }
+
+    @RequestMapping(
+            method = RequestMethod.GET,
+            path = "{userUid}"
+    )
+    public User fetchUser(@PathVariable("userUid")UUID userUid) {
+        return userService.getUser(userUid).get();
+    }
+}
