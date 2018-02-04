4619e00b9c9c5eaa94a51d7dda81e8fa2c79147d
Umut Uzun <umutu@evam.com>
2018-02-04 13:42:33 +0300

    Add error message

--- src/main/java/com/umut/learnspringboot/resource/UserResource.java
+++ src/main/java/com/umut/learnspringboot/resource/UserResource.java
@@ -3,12 +3,15 @@ package com.umut.learnspringboot.resource;
 import com.umut.learnspringboot.model.User;
 import com.umut.learnspringboot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 /**
@@ -39,7 +42,25 @@ public class UserResource {
             method = RequestMethod.GET,
             path = "{userUid}"
     )
-    public User fetchUser(@PathVariable("userUid")UUID userUid) {
-        return userService.getUser(userUid).get();
+    public ResponseEntity fetchUser(@PathVariable("userUid")UUID userUid) {
+        return userService.getUser(userUid).<ResponseEntity>map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND)
+                        .body(new ErrorMessage("user " + userUid + " was not found.")));
+    }
+
+    class ErrorMessage {
+        String errorMessage;
+
+        public ErrorMessage(String errorMessage) {
+            this.errorMessage = errorMessage;
+        }
+
+        public String getErrorMessage() {
+            return errorMessage;
+        }
+
+        public void setErrorMessage(String errorMessage) {
+            this.errorMessage = errorMessage;
+        }
     }
 }
