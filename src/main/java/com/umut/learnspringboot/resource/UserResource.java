e3d892bde39269af370c3fa3c4fcd63a0e928dda
Umut Uzun <umutu@evam.com>
2018-02-04 14:24:49 +0300

    Add post, put, delete methods

--- src/main/java/com/umut/learnspringboot/resource/UserResource.java
+++ src/main/java/com/umut/learnspringboot/resource/UserResource.java
@@ -4,11 +4,9 @@ import com.umut.learnspringboot.model.User;
 import com.umut.learnspringboot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -46,6 +44,40 @@ public class UserResource {
         return userService.getUser(userUid).<ResponseEntity>map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND)
                         .body(new ErrorMessage("user " + userUid + " was not found.")));
+    }
+
+    @RequestMapping(
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<Integer> insertNewUser(@RequestBody User user) {
+        int result = userService.insertUser(user);
+        return getIntegerResponseEntity(result);
+    }
+
+    private ResponseEntity<Integer> getIntegerResponseEntity(int result) {
+        if(result == 1) {
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.badRequest().build();
+    }
+
+    @RequestMapping(
+            method = RequestMethod.PUT,
+            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<Integer> updateUser(@RequestBody User user) {
+        int result = userService.updateUser(user);
+        return getIntegerResponseEntity(result);
+    }
+
+    @RequestMapping(
+            method = RequestMethod.DELETE,
+            path = "{userUid}"
+    )
+    public ResponseEntity<Integer> deleteUser(@PathVariable("userUid") UUID userUid) {
+        int result = userService.removeUser(userUid);
+        return getIntegerResponseEntity(result);
     }
 
     class ErrorMessage {
